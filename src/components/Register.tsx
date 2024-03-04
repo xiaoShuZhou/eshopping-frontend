824619405855e51591c4ecad9187f01836f42a64
Sean Zhou <51042748+xiaoShuZhou@users.noreply.github.com>
2024-03-05 01:40:32 +0200

    Avatar change

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch } from '../redux/hooks';
 import { Button, TextField, Box, Typography } from '@mui/material';
 import { register } from '../redux/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { uploadImage } from '../misc/uploadFileService';
 const Register: React.FC = () => {
   const dispatch = useAppDispatch();
   const [email, setEmail] = useState('');
@@ -11,6 +12,18 @@ const Register: React.FC = () => {
   const [name, setName] = useState('');
   const [avatar, setAvatar] = useState('');
   const navigate = useNavigate();
+  
+  const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files[0]) {
+      try {
+        const imageUrl = await uploadImage(files[0]);
+        setAvatar(imageUrl);
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -53,14 +66,19 @@ const Register: React.FC = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <TextField
-        margin="normal"
-        fullWidth
-        label="Avatar URL (optional)"
-        name="avatar"
-        value={avatar}
-        onChange={(e) => setAvatar(e.target.value)}
-      />
+            <Button
+              component="label"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Upload Avatar
+              <input
+                type="file"
+                hidden
+                onChange={handleImageUpload}
+              />
+            </Button>
       <Button
         type="submit"
         fullWidth
